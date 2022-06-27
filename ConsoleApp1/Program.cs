e5b10711bf84dd026f019e188237e0f630268643
Maxim Konev <sayberix.ru@yandex.ru>
2022-06-28 01:24:08 +0700

    create project for home work_5.2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
