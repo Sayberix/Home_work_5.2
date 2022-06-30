e73a15ea76ec292fd3458049c082f1d9b540b902
Maxim Konev <sayberix.ru@yandex.ru>
2022-06-30 21:41:03 +0700

    Выполнена третья задача к домашнему заданию урока 5.2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,17 @@ namespace ConsoleApp1
         {
             // Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-            
-
+            Console.WriteLine("Введите номер дня недели (от 1 до 7):");
+            int  dayOfWeek= Convert.ToInt32(Console.ReadLine());
+            if (dayOfWeek >= 1 && dayOfWeek <= 7)
+            {
+                if(dayOfWeek >= 1 && dayOfWeek <= 5)
+                    Console.Write("Заданный день недели не является выходным!");
+                else
+                    Console.Write("Заданный день недели является выходным!");
+            }
+            else
+                Console.WriteLine("Неверно введенный день недели!");
         }
     }
 }
