b6db9b1b0d62dd2b5aa1fa74988d400629e07167
Maxim Konev <sayberix.ru@yandex.ru>
2022-06-30 21:25:53 +0700

    Выполнена вторая задача к домашнему заданию урока 5.2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,11 +7,20 @@ namespace ConsoleApp1
         static void Main()
         {
             // Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+ 
+            Console.WriteLine("Введите трехзначное число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if ((number / 100) < 1)
+                Console.WriteLine("У данного числа третьей цифры нет!");
+            else
+            {
+                Console.Write("Третья цифра в заданом числе: " + digitThird(number));
 
-
-
-
-
+                int digitThird(int number)
+                {
+                    return (number % 10);
+                }
+            }
         }
     }
 }
